41a5dc35538c8d7a621235c09bcb191d870b7013
Guru <guru@Gurus-MacBook-Pro.local>
2024-09-18 11:00:39 +0530

    Added login box

--- src/App.js
+++ src/App.js
@@ -1,31 +1,9 @@
-import logo from './logo.svg';
 import './App.css';
-import PTagComponent from './PTagComponent';
-import { useState } from 'react';
+import Login from './Login';
 
 function App() {
-	const [fullName, changeFullName] = useState({
-		firstName: "",
-		lastName: ""
-	});
-
-	const handleChange = (e) => {
-		changeFullName({
-			...fullName,
-			[e.target.name]: e.target.value
-		});
-	}
-
 	return (
-		<div className="App">
-			<label> Frist Name: <input type="text" name="firstName" onChange={handleChange}/> </label>
-			<label> Last Name: <input type="text" name="lastName" onChange={handleChange}/> </label>
-			
-			<PTagComponent 
-				name={`${fullName.firstName} ${fullName.lastName}`} 
-				age={26} 
-			/>
-		</div>
+		<Login />
 	);
 }
 
---
+++ src/Login.js
@@ -0,0 +1,37 @@
+import React, {useState} from "react";
+
+const userCredentials ={
+    user: "avinash",
+    pass: "avinash@123"
+}
+
+export default function Login(){
+
+    const [userObj, setUserObj] = useState({
+        username: "",
+        password: ""
+    });
+
+    const handleChange = (event) => {
+       setUserObj({
+            ...userObj,
+            [event.target.name]: event.target.value
+       })
+    }
+
+    const handleSubmission = () => {
+        if(userObj.username === userCredentials.user && userObj.password === userCredentials.pass){
+            alert("Successfully Logged In.");
+        } else {
+            alert("Oops! Authentication Failed.")
+        }
+    }
+
+    return <>
+        <label>Username: <input name="username" value={userObj.username} type="text" onChange={handleChange}/> </label>
+        <br/>
+        <label>Password: <input name="password" value={userObj.password} type="password" onChange={handleChange}/> </label>
+        <br/>
+        <button onClick={handleSubmission}>Login</button>
+    </>
+}
